1b932df8735ac24d38d7e43bbf65003c5120eed0
maxime <maxbragh@gmail.com>
2020-03-02 17:18:50 +0100

    vitrine.1

---
+++ vitrine/php/view/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- CHARGE LE CODE CSS AVEC LA PAGE HTML -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h1>CONCESSIONNAIRE MERCO BENZ</h1>
+        <nav>
+            <a href="index.php">accueil</a>
+            <a href="catalogue.php">catalogue</a>
+            <a href="contact.php">contact</a>
+        </nav>
+    </header>
+    <main>
+
+        
+        
+
+    
